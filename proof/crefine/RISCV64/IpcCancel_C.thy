2fd599e0b14801961bb281097f1299718fcbb9a2
Nick Spinale <nick@nickspinale.com>
2024-11-14 21:34:25 -0800

    x

--- proof/crefine/RISCV64/IpcCancel_C.thy
+++ proof/crefine/RISCV64/IpcCancel_C.thy
@@ -2983,7 +2983,7 @@ apply wpc
 unfolding blockedCancelIPC_def
 apply (simp add: word_sle_def ccorres_cond_iffs Let_def cong: call_ignore_cong)
 
-              apply (simp add: bind_assoc)
+              apply (simp only: bind_assoc)
 
             apply (rule ccorres_symb_exec_l)
      apply (rule_tac P="epptr = x11" in ccorres_gen_asm2)
@@ -3001,7 +3001,7 @@ apply (simp add: word_sle_def ccorres_cond_iffs Let_def cong: call_ignore_cong)
                 apply (rule ccorres_rhs_assoc2)
                 apply (rule ccorres_rhs_assoc2)
                 apply (rule ccorres_rhs_assoc2)
-sorry
+
                 apply (ctac (no_vcg) add: cancelIPC_ccorres_helper)
                   apply (ctac add: setThreadState_ccorres_valid_queues')
                  apply (wp hoare_vcg_all_lift set_ep_valid_objs' | simp add: valid_tcb_state'_def split del: if_split)+
