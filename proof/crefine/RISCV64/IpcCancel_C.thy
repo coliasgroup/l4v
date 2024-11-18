053e652b49f0948b9d3dacdbe61b4cd924fca700
Nick Spinale <nick@nickspinale.com>
2024-11-18 02:38:58 -0800

    x

--- proof/crefine/RISCV64/IpcCancel_C.thy
+++ proof/crefine/RISCV64/IpcCancel_C.thy
@@ -2871,6 +2871,17 @@ lemma cancelIPC_ccorres_helper:
   by assumption
 
 lemma cancelIPC_ccorres_helper_x_2:
+(*
+  "ccorres dc xfdc
+    (invs' and (\<lambda>s. sym_refs (state_refs_of' s)) and st_tcb_at' (\<lambda>st. (isBlockedOnSend st \<or> isBlockedOnReceive st) \<and> replyObject st = replyOpt) thread)
+    {s. reply_' s = Ptr (option_to_0 replyOpt)}
+    []
+    (case replyOpt of None \<Rightarrow> return () | Some reply \<Rightarrow> replyUnlink reply thread)
+      (IF \<acute>reply \<noteq> NULL THEN
+        CALL reply_unlink(\<acute>reply,tcb_ptr_to_ctcb_ptr thread)
+    FI)"
+*)
+
   "ccorres dc xfdc
     (invs' and tcb_at' tcbPtr and reply_at' replyPtr)
     (\<lbrace>\<acute>tcb = tcb_ptr_to_ctcb_ptr tcbPtr\<rbrace> \<inter> \<lbrace>\<acute>reply = Ptr replyPtr\<rbrace>)
@@ -2879,8 +2890,21 @@ lemma cancelIPC_ccorres_helper_x_2:
     (IF \<acute>reply \<noteq> NULL THEN
         CALL reply_unlink(\<acute>reply, \<acute>tcb)
     FI)"
+
+(*
+  "ccorres dc xfdc
+    (invs' and tcb_at' tcbPtr and reply_at' replyPtr)
+    (\<lbrace>\<acute>tcb = tcb_ptr_to_ctcb_ptr tcbPtr\<rbrace> \<inter> \<lbrace>\<acute>reply = Ptr replyPtr\<rbrace>)
+    []
+    (case replyOpt of None \<Rightarrow> return () | Some replyPtr \<Rightarrow> replyUnlink replyPtr tcbPtr)
+    (IF \<acute>reply \<noteq> NULL THEN
+        CALL reply_unlink(\<acute>reply, \<acute>tcb)
+    FI)"
+*)
+(*
   apply (rule ccorres_from_vcg)
   apply (rule allI)
+*)
 sorry
 
 declare empty_fail_get[iff]
