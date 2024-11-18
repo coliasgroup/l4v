94883323d56255c7d56bb86b59cda6823754ad23
Nick Spinale <nick@nickspinale.com>
2024-11-18 05:00:06 -0800

    good

--- proof/crefine/RISCV64/IpcCancel_C.thy
+++ proof/crefine/RISCV64/IpcCancel_C.thy
@@ -3046,27 +3046,25 @@ lemma cancelIPC_ccorres1:
                     apply (rule_tac xf'=reply_' in ccorres_abstract, ceqv)
                     apply (rule_tac P="rv'b = option_to_ptr x13 \<and> x13 \<noteq> Some 0" in ccorres_gen_asm2)
                     apply (case_tac x13)
-
-(*
-defer
-apply simp
-apply wpsimp
-apply (ctac add: reply_unlink_ccorres)
-
-apply (ctac add: setThreadState_ccorres)
-
-(*
-
-*)
-*)
                     apply simp
-                      apply (ctac add: setThreadState_ccorres)
-apply simp
-(*apply wpsimp*)
+                    apply (ctac add: setThreadState_ccorres)
+                    apply simp
+
+(*
+apply (rule ccorres_guard_imp2)
+
+
 apply (ctac add: reply_unlink_ccorres)
+ apply (ctac add: setThreadState_ccorres)
+(*
+                    apply (simp add: ThreadState_defs)
 subgoal sorry
 
                      apply wpsimp
+*)
+*)
+subgoal sorry
+apply wpsimp
                     apply (simp add: ThreadState_defs)
                    apply vcg
                   apply (rule conseqPre, vcg)
