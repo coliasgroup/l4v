dfd4fe4e5e8cabdc3500168d1fc263190eaeed16
Nick Spinale <nick@nickspinale.com>
2024-11-19 03:31:36 -0800

    x

--- proof/crefine/RISCV64/IpcCancel_C.thy
+++ proof/crefine/RISCV64/IpcCancel_C.thy
@@ -3058,13 +3058,9 @@ prefer 2 subgoal sorry
                       apply (ctac add: setThreadState_ccorres)
                       \<comment> \<open>Some\<close>
                       apply simp
-                      apply (ctac add: reply_unlink_ccorres)
+                      apply (ctac (no_vcg) add: reply_unlink_ccorres)
                       apply (ctac add: setThreadState_ccorres)
                     apply wp
-
-subgoal sorry
-
-
                     apply (simp add: ThreadState_defs)
                    apply vcg
                   apply (rule conseqPre, vcg)
@@ -3074,12 +3070,6 @@ subgoal sorry
                  apply (rule subset_refl)
                 apply (rule conseqPre, vcg)
                 apply clarsimp
-
-                 apply (wp hoare_vcg_all_lift set_ep_valid_objs' | simp add: valid_tcb_state'_def split del: if_split)+
-                apply (simp add: ThreadState_defs)
-               apply vcg
-              apply (rule conseqPre, vcg)
-              apply clarsimp
 
                      apply wpsimp
 
