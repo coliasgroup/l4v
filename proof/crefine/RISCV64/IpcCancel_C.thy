1115c91c2d1572ad1f189144f6951470e5526151
Nick Spinale <nick@nickspinale.com>
2024-11-19 07:56:35 -0800

    x

--- proof/crefine/RISCV64/IpcCancel_C.thy
+++ proof/crefine/RISCV64/IpcCancel_C.thy
@@ -3097,7 +3097,7 @@ apply  (wpsimp wp: hoare_drop_imp hoare_vcg_all_lift | safe)
                           simp: valid_tcb_state'_def)+
 *)
 
-       
+(*
 
         apply (subst option.split[symmetric,where P=id, simplified]) (* see Ipc_C.thy *)
 
@@ -3108,6 +3108,7 @@ apply  (wpsimp wp: hoare_drop_imp hoare_vcg_all_lift | safe)
 apply (wp hoare_case_option_wp)
 
 apply (wp hoare_vcg_all_lift case_option_wp hoare_case_option_wp set_ep_valid_objs' | wpc | simp add: valid_tcb_state'_def split del: if_split)+
+*)
 
 subgoal sorry
 subgoal sorry
