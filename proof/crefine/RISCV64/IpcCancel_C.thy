70c7f3637019d1cf85126359eac1f479738426cb
Nick Spinale <nick@nickspinale.com>
2024-11-25 03:09:57 -0800

    x

--- proof/crefine/RISCV64/IpcCancel_C.thy
+++ proof/crefine/RISCV64/IpcCancel_C.thy
@@ -3084,7 +3084,7 @@ apply csymbr
                 apply (rule ccorres_rhs_assoc2)
                 apply (rule ccorres_rhs_assoc2)
                 apply (rule ccorres_rhs_assoc2)
-                apply (ctac (no_vcg) add: cancelIPC_ccorres_helper) 
+                apply (ctac (no_vcg) add: cancelIPC_ccorres_helper)
 (* todo: 2,3 *)
                 (* ! *)
 (*apply csymbr*)
@@ -3092,7 +3092,8 @@ apply csymbr
    apply (rule_tac xf'=ret__unsigned_longlong_'
             and val="option_to_0 ro"
             and R="valid_objs' and no_0_obj' and st_tcb_at' ((=) (BlockedOnReceive bo bicg ro)) thread"
-            and R'=UNIV
+            and R'=UNIV            
+            (* and R'="{s'. s \<Turnstile>\<^sub>c tcb_ptr_to_ctcb_ptr thread}" *)
             in ccorres_symb_exec_r_known_rv)
         apply clarsimp
        apply (rule conseqPre, vcg)
@@ -3242,6 +3243,7 @@ split: option.splits thread_state.splits
 )
 
 subgoal sorry
+
 
    apply clarsimp
   apply (drule(1) obj_at_cslift_tcb)
