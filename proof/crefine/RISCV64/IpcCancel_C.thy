ecd2d434e7e05461778bcea0bdc5f2521b9f80ea
Nick Spinale <nick@nickspinale.com>
2024-11-27 13:00:45 -0800

    x

--- proof/crefine/RISCV64/IpcCancel_C.thy
+++ proof/crefine/RISCV64/IpcCancel_C.thy
@@ -3455,6 +3455,15 @@ invs_valid_objs'
 valid_objs'_valid_tcbs'
 x_d
 )
+    subgoal by (auto simp: obj_at'_def projectKOs pred_tcb_at'_def invs'_def
+                     isTS_defs cte_wp_at_ctes_of
+                     cthread_state_relation_def sch_act_wf_weak valid_ep'_def
+                      sym_refs_asrt_def
+                      ready_qs_runnable_def
+                      invs_valid_objs'
+                      valid_objs'_valid_tcbs'
+                      x_d
+                     split: thread_state.splits)
 
    apply (rule conjI)
     apply (clarsimp simp: obj_at'_def projectKOs pred_tcb_at'_def invs'_def
