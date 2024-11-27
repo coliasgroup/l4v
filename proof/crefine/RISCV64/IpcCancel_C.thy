88fa216ddb282f3f97c988a3d9c11d420e2e223e
Nick Spinale <nick@nickspinale.com>
2024-11-27 13:28:21 -0800

    x

--- proof/crefine/RISCV64/IpcCancel_C.thy
+++ proof/crefine/RISCV64/IpcCancel_C.thy
@@ -3479,20 +3479,6 @@ apply (simp add: invs_valid_objs' x_d)
 
 
 
-apply (simp add: invs_valid_objs' x_d)
-
-    subgoal by (auto simp: obj_at'_def projectKOs pred_tcb_at'_def invs'_def
-                     isTS_defs cte_wp_at_ctes_of
-                     cthread_state_relation_def sch_act_wf_weak valid_ep'_def
-                      sym_refs_asrt_def
-                      ready_qs_runnable_def
-                      invs_valid_objs'
-                      valid_objs'_valid_tcbs'
-                      x_d
-                     split: thread_state.splits endpoint.splits)
-
-apply (simp add: invs_valid_objs' x_d)
-
 
           apply (clarsimp simp:
 sym_refs_asrt_def
@@ -3502,6 +3488,35 @@ valid_objs'_valid_tcbs'
 x_d
 )
 apply (case_tac xb)
+    apply clarsimp
+    apply (frule (3) ep_blocked_in_queueD_recv)
+    apply (frule (1) ko_at_valid_ep'[OF _ invs_valid_objs'])
+
+    subgoal by (auto simp: obj_at'_def projectKOs pred_tcb_at'_def invs'_def
+                     isTS_defs cte_wp_at_ctes_of isRecvEP_def isSendEP_def
+                     cthread_state_relation_def sch_act_wf_weak valid_ep'_def
+                      sym_refs_asrt_def
+                      ready_qs_runnable_def
+                      invs_valid_objs'
+                      valid_objs'_valid_tcbs'
+                      x_d
+                     split: thread_state.splits endpoint.splits)
+apply (simp add: invs_valid_objs' x_d)
+    apply clarsimp
+    apply (frule (3) ep_blocked_in_queueD_recv)
+    apply (frule (1) ko_at_valid_ep'[OF _ invs_valid_objs'])
+    subgoal by (auto simp: obj_at'_def projectKOs pred_tcb_at'_def invs'_def
+                     isTS_defs cte_wp_at_ctes_of isRecvEP_def isSendEP_def
+                     cthread_state_relation_def sch_act_wf_weak valid_ep'_def
+                      sym_refs_asrt_def
+                      ready_qs_runnable_def
+                      invs_valid_objs'
+                      valid_objs'_valid_tcbs'
+                      x_d
+                     split: thread_state.splits endpoint.splits)
+
+
+
 apply simp
 apply (rule conjI)
     subgoal by (auto simp: obj_at'_def projectKOs pred_tcb_at'_def invs'_def
