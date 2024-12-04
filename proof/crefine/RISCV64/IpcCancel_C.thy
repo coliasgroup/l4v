199450ba35687b3719a6483d56da6faccd800058
Nick Spinale <nick@nickspinale.com>
2024-12-04 13:34:03 -0800

    isar

--- proof/crefine/RISCV64/IpcCancel_C.thy
+++ proof/crefine/RISCV64/IpcCancel_C.thy
@@ -3003,8 +3003,9 @@ lemma cancelIPC_threadSet_wp:
               \<and> weak_sch_act_wf (ksSchedulerAction s) s \<and> sym_refs_asrt s\<rbrace>"
 proof -
   have sy: "threadSet (tcbFault_update (\<lambda>_. None)) t \<lbrace>sym_refs_asrt\<rbrace>"
-    by - (unfold sym_refs_asrt_def, wpsimp wp: threadSet_state_refs_of', simp,
-          (force simp: tcb_bound_refs'_def)+)
+    unfolding sym_refs_asrt_def
+    by (wpsimp wp: threadSet_state_refs_of')
+       (force simp: tcb_bound_refs'_def)+
   show ?thesis
     by (wpsimp wp: threadSet_fault_invs' threadSet_pred_tcb_no_state
                    weak_sch_act_wf_lift tcb_in_cur_domain'_lift sy)
