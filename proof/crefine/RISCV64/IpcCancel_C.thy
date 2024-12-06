020d8b8147af8270c1abccfddcf01eac9835ccee
Nick Spinale <nick@nickspinale.com>
2024-12-05 18:22:45 -0800

    x

--- proof/crefine/RISCV64/IpcCancel_C.thy
+++ proof/crefine/RISCV64/IpcCancel_C.thy
@@ -3058,13 +3058,9 @@ lemma reply_unlink_ccorres:
          apply (rule ccorres_symb_exec_l)
             apply (rule ccorres_stateAssert)
             apply (rule ccorres_move_c_guard_reply)
-
-
             apply (ctac add: updateReply_tcb_ccorres)
-
               apply (ctac add: setThreadState_ccorres)
-
-   apply (wpsimp wp: updateReply_valid_objs' )
+             apply (wpsimp wp: updateReply_valid_objs' )
             apply vcg
            apply (wp gts_inv')
           apply (wp gts_wp')
@@ -3075,13 +3071,7 @@ lemma reply_unlink_ccorres:
      apply wpsimp
     apply wp
    apply (simp add: getReply_def getObject_def split: option.splits)
-
-apply clarsimp
-apply (clarsimp
-simp:
-obj_at'_def
-reply_at'_def
-)
+  apply (clarsimp simp: st_tcb_at'_def obj_at'_def valid_reply'_def)
 
 
 
