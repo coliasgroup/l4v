2bd1d7352de82223a6324318b4a1eff4ab995052
Nick Spinale <nick@nickspinale.com>
2024-12-07 10:36:07 -0800

    x

--- proof/crefine/RISCV64/IpcCancel_C.thy
+++ proof/crefine/RISCV64/IpcCancel_C.thy
@@ -3082,6 +3082,46 @@ lemma updateReply_tcb_ccorres:
     (updateReply reply (replyTCB_update (\<lambda>_. None)))
     (Basic (\<lambda>s. globals_update (t_hrs_'_update
       (hrs_mem_update (heap_update (Ptr &(reply' s\<rightarrow>[''replyTCB_C''])) NULL))) s))"
+  apply (rule ccorres_guard_imp2)
+
+            apply (rule_tac
+(*
+Q="\<lambda>s reply. {s'. (s, s') \<in> rf_sr}"
+                        and R=UNIV
+                        and
+*)
+P=\<top>
+                        and P'=\<top>
+                         in updateReply_ccorres_lemma4)
+    apply vcg
+   prefer 2
+   apply (rule conjI, simp)
+   apply assumption
+  apply clarsimp
+
+  apply (clarsimp simp: rf_sr_def cstate_relation_def Let_def)
+  apply (clarsimp simp: cmachine_state_relation_def carch_state_relation_def cpspace_relation_def
+                        refill_buffer_relation_def)
+  apply (clarsimp simp: update_replies_map_tos typ_heap_simps')
+
+  apply (simp add: map_to_ctes_upd_tcb_no_ctes map_to_tcbs_upd tcb_cte_cases_def cteSizeBits_def)
+  apply (simp add: cep_relations_drop_fun_upd
+                   cvariable_relation_upd_const ko_at_projectKO_opt)
+  apply (drule ko_at_projectKO_opt)
+  apply (erule (2) cmap_relation_upd_relI)
+    subgoal by (simp add: ctcb_relation_def)
+   apply assumption
+  apply simp
+
+             apply (rule conseqPre, vcg)
+             apply clarsimp
+             apply (frule (1) obj_at_cslift_tcb)
+             apply (fastforce intro!: rf_sr_tcb_update_no_queue_gen2
+                                simp: typ_heap_simps' tcb_cte_cases_def cteSizeBits_def
+                                      ctcb_relation_def option_to_ctcb_ptr_def)
+            apply (clarsimp simp: ctcb_relation_def option_to_ctcb_ptr_def split: if_splits)
+           apply ceqv
+
 sorry
 
 lemma empty_fail_assert_opt:
